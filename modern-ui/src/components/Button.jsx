c0725cd354db31ac167329b72358ba2117605998
DaiyanNDahy <daiyanndahy@gmail.com>
2024-05-21 19:58:53 +0600

    button component

--- modern-ui/src/components/Button.jsx
+++ modern-ui/src/components/Button.jsx
@@ -4,13 +4,19 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-  const spanClasss = `relative z-10`;
+  const spanClasses = `relative z-10`;
   const renderButton = () => (
     <button className={classes}>
-      <span className={spanClasss}>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton();
 };
 export default Button;
