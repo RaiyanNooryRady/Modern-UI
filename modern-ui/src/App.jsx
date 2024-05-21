727a68f61162c7d3ff83b2ee32f08517aeb1c0a1
DaiyanNDahy <daiyanndahy@gmail.com>
2024-05-21 10:48:37 +0600

    assets

--- modern-ui/src/App.jsx
+++ modern-ui/src/App.jsx
@@ -1,15 +1,10 @@
-import './App.css'
-
 function App() {
-
   return (
     <>
-    <h1 className='text-light text-3xl'>Hello world</h1>
-    <div className='text-red-700'>
-      ok
-    </div>
+      <h1 className="text-light text-3xl">Hello world</h1>
+      <div className="text-red-700 font-bold underline">ok</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
