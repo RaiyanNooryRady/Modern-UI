fc684ac7f0a2761ff8a5c2728e05c5eb28d5123a
DaiyanNDahy <daiyanndahy@gmail.com>
2024-05-22 06:06:55 +0600

    header done

--- modern-ui/src/App.jsx
+++ modern-ui/src/App.jsx
@@ -1,19 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-light text-3xl">Hello world</h1>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className=" mt-5 " href="#login">
-          Submit
-        </Button>
-        ok
-      </div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt fugit
-      voluptatum nam aliquam quam consequuntur ipsum assumenda saepe quasi
-      cupiditate omnis, distinctio neque in sapiente! Itaque, eum veniam?
-      Laboriosam, mollitia?
+      <Header />
       <ButtonGradient />
     </>
   );
